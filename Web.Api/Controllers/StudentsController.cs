2de474b75d42da872b445df75e44122671bedd99
nadiardgs <nadia.rodrigues93@gmail.com>
2026-03-26 23:51:18 +0000

    feat: changed StudentsController.cs to primary constructor

--- Web.Api/Controllers/StudentsController.cs
+++ Web.Api/Controllers/StudentsController.cs
@@ -7,30 +7,20 @@ namespace WebApplication3.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class StudentsController : ControllerBase
+public class StudentsController(CollegeDbContext context, IValidator<Student> validator) : ControllerBase
 {
-    private readonly CollegeDbContext _context;
-    private readonly IValidator<Student> _validator;
-
-    public StudentsController(CollegeDbContext context, IValidator<Student> validator)
-    {
-        _context = context;
-        _validator = validator;
-    }
-
     [HttpPost]
     public async Task<ActionResult> Create(Student student)
     {
-        // Explicit async validation
-        var validationResult = await _validator.ValidateAsync(student);
+        var validationResult = await validator.ValidateAsync(student);
 
         if (!validationResult.IsValid)
         {
             return BadRequest(validationResult.Errors);
         }
 
-        _context.Students.Add(student);
-        await _context.SaveChangesAsync();
+        context.Students.Add(student);
+        await context.SaveChangesAsync();
         return Ok(student);
     }
 }
